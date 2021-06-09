fa9b4984c31ce0415b81bc47585272f941dfb2d4
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-09 11:46:49 +0530

    Added:Coin is flipped multiple times, heads and tails displayed

--- coinSim.sh
+++ coinSim.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result -eq 1 ]
-then
-	echo Tails won
-else
-	echo Heads won
-fi
+heads=0
+tails=0
+for((i=0;i<5;i++))
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 1 ]
+	then
+		((tails++))
+		echo Tails won
+	else
+		echo Heads won
+		((heads++))
+	fi
+done
+echo "Heads won:" $heads
+echo "Tails won:" $tails
