b219af321c71de002fb3d52188221ceaf13d76db
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-10 11:28:40 +0530

    Added:Looped till either reaches 21 and extended if tie

--- coinSim.sh
+++ coinSim.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 heads=0
 tails=0
-for((i=0;i<5;i++))
+min_difference=2
+target_count=21
+while [ 1 ]
 do
 	result=$((RANDOM%2))
 	if [ $result -eq 1 ]
@@ -11,6 +13,21 @@ do
 	else
 		echo Heads won
 		((heads++))
+	fi
+	if [ $heads -gt $tails ]
+	then
+		diff=$(($heads-$tails))
+	else
+		diff=$(($tails-$heads))
+	fi
+	if [[ $heads -eq $target_count && $diff -ge $min_difference ]]
+	then
+		echo "Heads won by" $diff
+		break
+	elif [[ $tails -eq $target_count && $diff -ge $min_difference ]]
+		then
+		echo "Tails won by" $diff
+		break
 	fi
 done
 echo "Heads won:" $heads
